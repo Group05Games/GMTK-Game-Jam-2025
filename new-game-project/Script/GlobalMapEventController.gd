590f07fbcea5d6fe1ee297e2c62524395abc5ae1
Ben <bencrimmins8@gmail.com>
2025-07-31 22:38:17 -0400

    Update GlobalMapEventController.gd

--- new-game-project/Script/GlobalMapEventController.gd
+++ new-game-project/Script/GlobalMapEventController.gd
@@ -6,13 +6,16 @@ var count = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	add_child(timer)
-	timer.start(1)
+	timer.start(5)
 	timer.connect("timeout", on_global_timer_timeout)
 
 
+# Called every time the global timer times out
 func on_global_timer_timeout():
-	count += 1
-	print(count)
+	# Randomly pick a relevant tile
+	
+	# Randomly pick a number that must match with that tile
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
