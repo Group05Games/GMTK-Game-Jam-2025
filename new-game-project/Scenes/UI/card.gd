fdcdd814b4f546d9c7c5775cf77a85e96d5b074f
RyanA3 <alsobrooks.ryan@gmail.com>
2025-08-01 23:44:12 -0400

    Card selecting

--- new-game-project/Scenes/UI/card.gd
+++ new-game-project/Scenes/UI/card.gd
@@ -1,1 +1,12 @@
 extends Control
+
+@export var card_type = "0"
+
+func _ready():
+	pass
+	
+func set_type(type: String) -> void:
+	self.card_type = type;
+	name = GlobalSettings.TileDictionary[type]["Name"]
+	var label = get_child(0).get_child(1) as Label
+	label.text = name
