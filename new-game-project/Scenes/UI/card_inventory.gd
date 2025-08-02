e2bbd26ab27806a1bc4a2caf1b0b5967592cd935
RyanA3 <alsobrooks.ryan@gmail.com>
2025-08-02 00:50:04 -0400

    Tweeng

--- new-game-project/Scenes/UI/card_inventory.gd
+++ new-game-project/Scenes/UI/card_inventory.gd
@@ -7,28 +7,44 @@ var cardsInventory: Dictionary
 @onready var card_scene = preload("res://Scenes/UI/Card.tscn")
 
 var selected_card_instance_id = 0
+var is_open = false
+var tween: Tween
+var menu_panel_original_position: Vector2
+const LIFT_AMOUNT = 192
 
-# Contains all the card objects
-var container: HBoxContainer
+@onready var menu_panel = $MenuPanel
+@onready var container = $MenuPanel/CardContainer
+@onready var deselect_card_button = $MenuPanel/DeselectButton
+@onready var open_menu_hover = $OpenMenuHover
+@onready var hold_open_menu_hover = $HoldOpenMenuHover
 
 func _ready():
 	# Init card inventory
 	for i in range(10):
 		cardsInventory[str(i)] = 0
 	
-	container = self.get_child(0)
-	self.get_child(1).gui_input.connect(on_deselect)
+	self.mouse_entered.connect(input_passforward)
+	open_menu_hover.mouse_entered.connect(on_open_mouse_entered)
+	hold_open_menu_hover.mouse_exited.connect(on_hold_open_mouse_exited)
+	
+	deselect_card_button.gui_input.connect(on_deselect_button)
+	
+	menu_panel_original_position = menu_panel.position
 
 	for i in range(container.get_child_count()):
 		print("Loop child " + str(i))
-		container.get_child(i).gui_input.connect(on_mouse_pressed.bind(container.get_child(i).get_instance_id()))
+		container.get_child(i).gui_input.connect(on_card_clicked.bind(container.get_child(i).get_instance_id()))
 		
 	add_card("1", 1)
 	add_card("0", 2)
 	add_card("1", 1)
 	add_card("3", 1)
 	add_card("1", 1)
-	
+
+func input_passforward(event: InputEvent):
+	print("GOT EVENT")
+	container._on_parent_click(event)
+	deselect_card_button._on_parent_click(event)
 
 # Add or remove cards to the inventory
 func add_card(type: String, amount: int) -> void:
@@ -56,11 +72,27 @@ func add_card(type: String, amount: int) -> void:
 	for i in range(amount):
 		var card = card_scene.instantiate()
 		card.set_type(type)
-		card.gui_input.connect(on_mouse_pressed.bind(card.get_instance_id()))
+		card.gui_input.connect(on_card_clicked.bind(card.get_instance_id()))
 		container.add_child(card)
 		container.move_child(card, insert)
 		
-func on_mouse_pressed(event: InputEvent, instance_id: int) -> void:
+func select_card(instance_id: int) -> void:
+	if self.selected_card_instance_id != 0:
+		var previously_selected = instance_from_id(self.selected_card_instance_id) as Control
+		previously_selected.modulate = Color("ffffffff")
+	
+	self.selected_card_instance_id = instance_id
+	
+	var selected = instance_from_id(self.selected_card_instance_id)
+	if selected:
+		selected.modulate = Color("999999ff")
+
+
+
+############
+# HANDLERS #
+############
+func on_card_clicked(event: InputEvent, instance_id: int) -> void:
 	if event is not InputEventMouseButton:
 		return
 		
@@ -72,7 +104,7 @@ func on_mouse_pressed(event: InputEvent, instance_id: int) -> void:
 	select_card(instance_id)
 	#add_card(card.card_type, -1)
 	
-func on_deselect(event: InputEvent) -> void:
+func on_deselect_button(event: InputEvent) -> void:
 	if event is not InputEventMouseButton:
 		return
 		
@@ -82,13 +114,27 @@ func on_deselect(event: InputEvent) -> void:
 	print("DESELECT CARD")
 	select_card(0)
 	
-func select_card(instance_id: int) -> void:
-	if self.selected_card_instance_id != 0:
-		var previously_selected = instance_from_id(self.selected_card_instance_id) as Control
-		previously_selected.modulate = Color("ffffffff")
+func on_open_mouse_entered() -> void:
+	# Nothing to do
+	if(self.is_open):
+		return
+	self.is_open = true
 	
-	self.selected_card_instance_id = instance_id
+	if tween and tween.is_running():
+		tween.kill()
+	tween = create_tween()
+	tween.tween_property(menu_panel, "position", menu_panel_original_position, 0.2)
+	#menu_panel.position = menu_panel_original_position
 	
-	var selected = instance_from_id(self.selected_card_instance_id)
-	if selected:
-		selected.modulate = Color("999999ff")
+func on_hold_open_mouse_exited() -> void:
+	# Nothing to do
+	if(!self.is_open):
+		return
+	self.is_open = false
+	
+	if tween and tween.is_running():
+		tween.kill()
+	tween = create_tween()
+	tween.tween_property(menu_panel, "position", menu_panel_original_position + Vector2(0, LIFT_AMOUNT), 0.2)
+	#menu_panel.position = menu_panel_original_position + Vector2(0, LIFT_AMOUNT)
+	
