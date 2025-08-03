7d8d75ec46decb5fe4ba69154a5b933af7d5238b
Group05Games <109384258+Group05Games@users.noreply.github.com>
2025-08-02 22:28:05 -0400

    Move Caravan to prefabs

--- new-game-project/Script/Caravan.gd
+++ new-game-project/Script/Caravan.gd
@@ -1,5 +1,10 @@
 extends Node
 
+@onready var path = $Path2D2
+@onready var path_follow = $Path2D2/PathFollow2D
+@onready var caravan = $Path2D2/PathFollow2D/Caravan
+@onready var timer = $Path2D2/PathFollow2D/Caravan/Timer
+
 var CaravanInventory : Inventory = Inventory.new()
 
 func _ready():
@@ -13,3 +18,14 @@ func depositAsGold():
 	CaravanInventory.convertToGold()
 	GlobalSettings.CityInventory.addInventory(CaravanInventory.inventory)
 	CaravanInventory.emptyInventory()
+
+func definePath():
+	#While active should:
+	#Take user left clicks on neighboring nodes
+	#Add node to the path if it is not already part of the path
+	#Continue from the last node selected
+	#Left click on already selected node should deselect that node and all child nodes
+	#If the head of the list and the tail of he list are the same node, we have completed the loop.
+	#  #Save the path running between all points
+	
+	pass
