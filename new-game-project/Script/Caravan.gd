ffface1ca54f9bffd1149044eea2f51fb9f3b893
Group05Games <109384258+Group05Games@users.noreply.github.com>
2025-08-02 21:08:52 -0400

    Re-structure Caravan

--- new-game-project/Script/Caravan.gd
+++ new-game-project/Script/Caravan.gd
@@ -6,6 +6,10 @@ func _ready():
 	CaravanInventory.initInv()
 
 func deposit():
+	GlobalSettings.CityInventory.addInventory(CaravanInventory.inventory)
+	CaravanInventory.emptyInventory()
+
+func depositAsGold():
 	CaravanInventory.convertToGold()
 	GlobalSettings.CityInventory.addInventory(CaravanInventory.inventory)
 	CaravanInventory.emptyInventory()
